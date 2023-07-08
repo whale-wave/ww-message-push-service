3dbfd98eb0aaff42fefe9df253dc7f0fd719b31f
Avan <layouwen@gmail.com>
2023-07-08 22:22:18 +0800

    refactor: get text

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,12 +4,42 @@ export * from './logger';
 export * from './webhook';
 export * from './response';
 
-const mergeOption = (objValue: any, srcValue: any) => {
+function mergeOption(objValue: any, srcValue: any) {
   if (_.isArray(objValue)) {
     return _.uniq(objValue.concat(srcValue));
   }
+}
+
+export function merge(a: any, b: any) {
+  return _.mergeWith(a, b, mergeOption);
+}
+
+type GitMsgData = {
+  type: string;
+  platform: string;
+  repo: { url: string; name: string };
+  user: string;
+  commits: { url: string; no: string; note: string }[];
 };
 
-export const merge = (a: any, b: any) => {
-  return _.mergeWith(a, b, mergeOption);
-};
+function getTelegramMessage({ type, platform, repo, user, commits }: GitMsgData) {
+  return `*${type}*
+平台: ${platform}
+仓库: [${repo.name}](${repo.url})
+用户: ${user}
+提交信息:
+${commits
+  .map(commit => {
+    return `[${commit.no}](${commit.url}) ${commit.note}`;
+  })
+  .join('\n')}`.replaceAll('-', '\\-');
+}
+
+export function getGitText({ data, target = 'telegram' }: { data: GitMsgData; target?: 'telegram' }) {
+  switch (target) {
+    case 'telegram':
+      return getTelegramMessage(data);
+    default:
+      return '';
+  }
+}
