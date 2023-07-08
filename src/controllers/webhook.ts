09ec96f44ef203383f34c313c357a739ba1faaf3
Avan <layouwen@gmail.com>
2023-07-08 22:46:21 +0800

    chore: add logger

--- src/controllers/webhook.ts
+++ src/controllers/webhook.ts
@@ -34,68 +34,74 @@ class WebhookController {
   }
 
   async github(req: Request, res: Response) {
-    const { head_commit, commits, repository, hook_id } = req.body;
+    try {
+      const { head_commit, commits, repository, hook_id } = req.body;
 
-    let result: any = null;
+      let result: any = null;
 
-    const project = {
-      name: repository.name,
-      url: repository.html_url,
-      branch: repository.default_branch,
-    };
+      const project = {
+        name: repository.name,
+        url: repository.html_url,
+        branch: repository.default_branch,
+      };
 
-    if (hook_id && repository) {
-      result = getFeiShuPostByTemplate('update', 'gitlab', project, [
-        [
-          {
-            tag: 'text',
-            text: 'webhook update',
-          },
-        ],
-      ]);
-    } else if (head_commit && repository && commits) {
-      result = toFeiShu(
-        {
-          title: 'push',
-          project,
-          user_name: head_commit.author.name,
-          user_username: head_commit.author.username,
-          commits,
-        },
-        'github'
-      );
-
-      await telegramService.sendMessage(
-        getGitText({
-          data: {
-            type: 'push',
-            platform: 'github',
-            repo: {
-              url: repository.html_url,
-              name: repository.name,
+      if (hook_id && repository) {
+        result = getFeiShuPostByTemplate('update', 'gitlab', project, [
+          [
+            {
+              tag: 'text',
+              text: 'webhook update',
             },
-            user: head_commit.author.name,
-            commits: commits.map((commit: any) => {
-              return {
-                url: commit.url,
-                no: commit.id.slice(0, 6),
-                note: commit.message,
-              };
-            }),
+          ],
+        ]);
+      } else if (head_commit && repository && commits) {
+        result = toFeiShu(
+          {
+            title: 'push',
+            project,
+            user_name: head_commit.author.name,
+            user_username: head_commit.author.username,
+            commits,
           },
-        })
-      );
-    } else {
-      logger.daily.info('github no support type', req.body);
-      return res.json(new ErrorResponse({ message: 'no support' }));
-    }
+          'github'
+        );
 
-    for (const url of config.webhooks.feiShu) {
-      const { data } = await axios.post(url, result);
-      logger.daily.info('github to fei shu result', data);
-    }
+        const tgRes = await telegramService.sendMessage(
+          getGitText({
+            data: {
+              type: 'push',
+              platform: 'github',
+              repo: {
+                url: repository.html_url,
+                name: repository.name,
+              },
+              user: head_commit.author.name,
+              commits: commits.map((commit: any) => {
+                return {
+                  url: commit.url,
+                  no: commit.id.slice(0, 6),
+                  note: commit.message,
+                };
+              }),
+            },
+          })
+        );
+        logger.daily.info('github to telegram result', tgRes);
+      } else {
+        logger.daily.info('github no support type', req.body);
+        return res.json(new ErrorResponse({ message: 'no support' }));
+      }
 
-    res.json(new SuccessResponse());
+      for (const url of config.webhooks.feiShu) {
+        const { data } = await axios.post(url, result);
+        logger.daily.info('github to fei shu result', data);
+      }
+
+      res.json(new SuccessResponse());
+    } catch (e) {
+      logger.error.error('github webhook error', e);
+      res.json(new ErrorResponse({ message: 'catch error' }));
+    }
   }
 }
 
