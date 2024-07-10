4911e4d2b17a43796cb97e8d6b18f8eeb95c1451
Avan <layouwen@gmail.com>
2024-07-10 16:28:53 +0800

    fix: judge

--- src/services/gitlab.ts
+++ src/services/gitlab.ts
@@ -4,12 +4,25 @@ class GitlabService {
     tag_push: '推送标签',
   } as Record<string, string>;
 
-  public isDelete(after: string): boolean {
-    return Number(after) === 0;
+  public isPushBranch(before: string, after: string): boolean {
+    return Number(before) === 0 && Number(after) !== 0;
   }
 
-  public getType(object_kind: string, after: string): string {
-    return this.isDelete(after) ? '删除分支' : this.kindToTitleMap[object_kind] || object_kind;
+  public isDeleteBranch(before: string, after: string): boolean {
+    return Number(before) !== 0 && Number(after) === 0;
+  }
+
+  public getType(data: any): string {
+    const { object_kind, before, after } = data;
+
+    if (this.isPushBranch(before, after)) {
+      return '创建分支';
+    }
+    if (this.isDeleteBranch(before, after)) {
+      return '删除分支';
+    }
+
+    return this.kindToTitleMap[object_kind] || object_kind;
   }
 }
 
